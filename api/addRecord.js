b30ec4d3c96575929395873e968a33898393b385
mmartin1995 <molnmartin.1995@gmail.com>
2023-10-14 16:54:29 +0200

    Supabase-teszt

---
+++ api/addRecord.js
@@ -0,0 +1,32 @@
+// api/addRecord.js
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://abbftdocmrsdnwxingyn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiYmZ0ZG9jbXJzZG53eGluZ3luIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyOTQyMzUsImV4cCI6MjAxMjg3MDIzNX0.6bcaVBEZPozM2-n63jgSImn22H5GbycjcCykwesM2cc';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const { name /* további mezők */ } = req.body;
+
+            // Új rekord hozzáadása a Supabase adatbázisba
+            const { data, error } = await supabase
+                .from('sarkanylovas-teszt')
+                .insert([{ name /* további mezők */ }]);
+
+            if (error) {
+                res.status(500).json({ error: 'Hiba történt a rekord hozzáadása közben.' });
+            } else {
+                res.status(200).json({ message: 'Rekord sikeresen hozzáadva.' });
+            }
+        } catch (error) {
+            console.error('Hiba történt:', error);
+            res.status(500).json({ error: 'Hiba történt a rekord hozzáadása közben.' });
+        }
+    } else {
+        res.status(405).json({ error: 'Az HTTP metódus nem támogatott.' });
+    }
+}
---
+++ your_script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('newRecordForm');
+    
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        // További mezők értékeit is be kell olvasni
+
+        try {
+            const response = await fetch('/api/addRecord', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name /* további mezők */ }),
+            });
+
+            if (response.ok) {
+                alert('Rekord sikeresen hozzáadva.');
+                form.reset();
+            } else {
+                alert('Hiba történt a rekord hozzáadása közben.');
+            }
+        } catch (error) {
+            console.error('Hiba történt:', error);
+        }
+    });
+});
